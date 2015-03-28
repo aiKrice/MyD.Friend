15f0285da89d61111f89437f7258a02b67211f4e
Ludaa <ludovic.zamord@gmail.com>
2015-03-28 15:53:16 +0100

    Removing routes from app.js

--- e-sante-nodeapp/server/app.js
+++ e-sante-nodeapp/server/app.js
@@ -4,18 +4,12 @@
 /*jslint node:true */
 "use strict";
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-
-var routes = require('./routes/index');
-var community = require('./routes/landing_community');
-var product = require('./routes/landing_product');
-var history = require('./routes/history');
+var express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser');
 
 var app = express();
 
@@ -27,50 +21,11 @@ app.set('view engine', 'jade');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/landing-community', community);
-app.use('/landing-product', product);
-app.use('/history', history);
-
-// Connect to our mongo database
-mongoose.connect('mongodb://localhost/eSante', function (err) {
-    console.log("Connected to database");
-});
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
+// Load routes
+require('./config/routes')(app);
 
 module.exports = app;
